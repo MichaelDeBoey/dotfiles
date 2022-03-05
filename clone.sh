0217e7adcdc826626e1d8ee937b755d07eac6a4b
Michaël De Boey <info@michaeldeboey.be>
2022-03-06 00:15:03 +0100

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -4,8 +4,8 @@ echo "Cloning repositories..."
 
 DEVELOPER=$HOME/Developer
 REMARKEMBEDDER=$DEVELOPER/@remark-embedder
-CODESANDBOX=$DEVELOPER/CodeSandbox
 KENTCDODDS=$DEVELOPER/KentCDodds
+TESTINGLIBRARY=$DEVELOPER/TestingLibrary
 
 # Personal
 git clone git@github.com:MichaelDeBoey/gatsby-plugin-instagram-embed.git $DEVELOPER/gatsby-plugin-instagram-embed
@@ -19,11 +19,14 @@ git clone git@github.com:remark-embedder/core.git $REMARKEMBEDDER/core
 git clone git@github.com:remark-embedder/transformer-codesandbox.git $REMARKEMBEDDER/transformer-codesandbox
 git clone git@github.com:remark-embedder/transformer-oembed.git $REMARKEMBEDDER/transformer-oembed
 
-# CodeSandbox
-git clone git@github.com:codesandbox/codesandbox-client.git $CODESANDBOX/codesandbox-client
-git clone git@github.com:codesandbox/status-page.git $CODESANDBOX/status-page
-
 # Kent C. Dodds
 git clone git@github.com:kentcdodds/eslint-config-kentcdodds.git $KENTCDODDS/eslint-config-kentcdodds
 git clone git@github.com:kentcdodds/generator-kcd-oss.git $KENTCDODDS/generator-kcd-oss
 git clone git@github.com:kentcdodds/kcd-scripts.git $KENTCDODDS/kcd-scripts
+
+# Remix
+git clone git@github.com:remix-run/remix.git $DEVELOPER/remix
+
+# Testing Library
+git clone git@github.com:testing-library/eslint-plugin-testing-library.git $TESTINGLIBRARY/eslint-plugin-testing-library
+git clone git@github.com:testing-library/eslint-plugin-jest-dom.git $TESTINGLIBRARY/eslint-plugin-jest-dom

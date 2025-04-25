d1dbc6290065c62a7c4790ce165cb69a26c7f627
Michaël De Boey <info@michaeldeboey.be>
2025-04-25 10:03:47 +0200

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -2,6 +2,14 @@
 
 echo "Setting up your Mac..."
 
+# Check if Xcode Command Line Tools are installed
+if ! xcode-select -p &>/dev/null; then
+  echo "Xcode Command Line Tools not found. Installing..."
+  xcode-select --install
+else
+  echo "Xcode Command Line Tools already installed."
+fi
+
 # Check for Homebrew and install if we don't have it
 if test ! $(which brew); then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
